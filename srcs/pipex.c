0abfbf47bf247b6167eef7547b9b03ea7a1d27c0
Francisco Pires <98482270+PireXa@users.noreply.github.com>
2022-06-14 10:49:34 +0100

    Add files via upload

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -2,24 +2,27 @@
 
 int main(int ac, char **av, char **envp)
 {
-  int fd[2];
-  int pid1;
-  int pid2;
+	int fd[2];
+	int pid1;
+	int pid2;
 	int  fdin;
 	int  fdout;
-  char  *cmd1;
-  char  *cmd2;
+	char  **cmd1;
+	char  **cmd2;
 //	char *cmd1[] = {"/bin/ls", "-a",NULL};
 //	char *cmd2[] = {"/usr/bin/cat", "-e",NULL};
-
-  (void)ac;
-  cmd1 = (char *)malloc(31);
-  cmd2 = (char *)malloc(31);
-  cmd1 = check_commands(av[2], envp);
-  cmd2 = check_commands(av[3], envp);
-  printf("wtf\n");
-  if (pipe(fd) == -1)
-       	exit(0);
+	(void)ac;
+//	cmd1 = (char **)malloc(sizeof(char *));
+//	cmd2 = (char **)malloc(sizeof(char *));
+	cmd1 = ft_split(av[2], ' ');
+	cmd2 = ft_split(av[3], ' ');
+//	cmd1[0] = (char *)malloc(31);
+//	cmd2[0] = (char *)malloc(31);
+	cmd1[0] = check_commands(av[2], envp);
+	printf("comando %s\nopcao %s\n", cmd1[0], cmd1[1]);
+	cmd2[0] = check_commands(av[3], envp);
+	if (pipe(fd) == -1)
+		exit(0);
 	pid1 = fork();
 	if (pid1 == 0)
 	{
@@ -28,7 +31,7 @@ int main(int ac, char **av, char **envp)
 		dup2(fd[1], 1);
 		close(fd[0]);
 		close(fd[1]);
-		execve(cmd1, &cmd1, envp);
+		execve(cmd1[0], cmd1, envp);
 		close(fdin);
 	}
 	pid2 = fork();
@@ -39,7 +42,7 @@ int main(int ac, char **av, char **envp)
 		dup2(fd[0], 0);
 		close(fd[0]);
 		close(fd[1]);
-		execve(cmd2, &cmd2, envp);
+		execve(cmd2[0], cmd2, envp);
 		close(fdout);
 	}
 	close(fd[0]);
